73d1749653092ceaf845cba400764a04d74afc0a
IdealSoftSolutions <UserAdmin@123>
2024-07-08 17:11:51 -0500

    add header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,8 +2,27 @@ import React from 'react'
 import './Header.css'
 export default function Header() {
     return (
-        <div className='header'>
-            <label>I'm header</label>
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="header" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="header_nav">
+                            <a className="nav-link active" aria-current="page" href="#">HOME</a>
+                        </li>
+                        <li className="header_nav">
+                            <a className="nav-link" href="#">MENU</a>
+                        </li>
+                        <li className="header_nav">
+                            <a className="nav-link active" aria-current="page" href="#">ABOUT</a>
+                        </li>
+                        <li className="header_nav">
+                            <a className="nav-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+
         </div>
     )
 }
