7fbf225a7a5ceb4187c2dada574a44235c8f389e
IdealSoftSolutions <UserAdmin@123>
2024-07-15 15:42:30 -0500

    food

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,43 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Header.css'
-import { FaSearch } from "react-icons/fa"; // Import your desired icon
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Header() {
+    const [selectedMenu, setSelectedMenu] = useState('');
+
+    function handleClick(e) {
+        setSelectedMenu(e);
+    };
+
+
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="header" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="header_nav">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="#">On Tap</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="#">Wine & Spirits</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="#">Non Alcoholic</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="/menu">Food Menu</a>
-                        </li>
-                        <li className="header_nav">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                        <li className="header_nav">
-                            <input className="header_search_button" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-info" type="submit"><FaSearch /></button>
-                        </li>
-                    </ul>
+        <div className="navbar">
+            <div className="navbar-left">
+                <div className="navbar-links">
+                    <a itemProp='url' role='tab' aria-controls='primarypanel2' aria-selected='true' className={selectedMenu === 'home' ? 'activeMenu' : ''} onClick={() => handleClick('home')} href="/home">Home</a>
+                    <a className={selectedMenu === 'about' ? 'activeMenu' : ''} onClick={() => handleClick('about')} href="#">About</a>
+                    <a className={selectedMenu === 'menu' ? 'activeMenu' : ''} onClick={() => handleClick('menu')} href="/menu">Menu</a>
+                    <a className={selectedMenu === 'on tap' ? 'activeMenu' : ''} onClick={() => handleClick('on tap')} href="#">On Tap</a>
+                    <a className={selectedMenu === 'wine & spirits' ? 'activeMenu' : ''} onClick={() => handleClick('wine & spirits')} href="#">Wine & Spirits</a>
+                    <a className={selectedMenu === 'non alcoholic' ? 'activeMenu' : ''} onClick={() => handleClick('non alcoholic')} href="#">Non Alcoholic</a>
                 </div>
+            </div>
+            <div className="navbar-right">
+                <div className="navbar-icons">
+                    <div className="input-group rounded">
+                        <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                        <button type="button" className="btn btn-link" onClick={() => alert('Search icon clicked')}>
+                            <i className="fas fa-search" style={{ fontSize: '24px', color: 'white' }}></i>
+                        </button>
+                    </div>
 
-            </nav>
-
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,7 +1,57 @@
 import React from 'react'
+import cocktail from '../Assets/chicken/'
+import FoodCard from './FoodCard';
 
 export default function Menu() {
+
+
+    const foodItems = [
+        {
+            image: "",
+            title: "Paneer 65",
+            description: "Crispy Tamilian cottage cheese fritters marinated with garlic, chilly, curry leaves and assorted spices.",
+            price: "495",
+            isVegetarian: true
+        },
+        {
+            image: cocktail,
+            title: "Falafel & Hummus Platter",
+            description: "Chickpea Falafels served with Hummus and Pita.",
+            price: "495",
+            isVegetarian: true
+        },
+        {
+            image: cocktail,
+            title: "Vegetable Adana Kebab Platter",
+            description: "Turkish style vegetable skewers cooked in a tandoor.",
+            price: "1195",
+            isVegetarian: true
+        },
+        {
+            image: cocktail,
+            title: "Chicken 65",
+            description: "Crispy Tamilian chicken fritters marinated with garlic, chilly, curry leaves and assorted spices.",
+            price: "595",
+            isVegetarian: false
+        }
+    ];
+
+
     return (
-        <h2>Welcome to menu</h2>
+        <div className="catlist" pro='1'>
+            <header className="App-header">
+                <h1>Food Menu</h1>
+                <div className="container">
+                    <div className="row">
+                        {foodItems.map((item, index) => (
+                            <div key={index} className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+                                <FoodCard {...item} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </header>
+        </div>
+
     )
 }
